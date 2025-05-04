ebf59708635681c5ebb868491f9dfb48e2bdae77
Ovo446 <yuey1673@gmail.com>
2025-05-04 00:04:17 -0400

    Delete still-learning(2) directory

--- still-learning(2)/README.md
+++
@@ -1,3 +0,0 @@
-# Still Learning
-
-A simple site for practicing English reading.
--- still-learning(2)/pages/about.js
+++
@@ -1,9 +0,0 @@
-
-export default function About() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>About This Site</h1>
-      <p>This is a simple English learning web app.</p>
-    </div>
-  );
-}
